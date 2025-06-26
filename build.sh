17aaa6892930c937002b5704e176314f85422a9c
Anzhelika <darkl11n3ss@gmail.com>
2025-06-27 00:40:59 +0300

    Fix uv not found on Render

--- build.sh
+++ build.sh
@@ -3,13 +3,14 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 export PATH="$HOME/.local/bin:$PATH"
-source "$HOME/.local/bin/env"
 
-uv venv .venv
+$HOME/.local/bin/uv venv .venv
+
 source .venv/bin/activate
 
-uv pip install .
+$HOME/.local/bin/uv pip install .
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
 
