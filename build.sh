d3e015b29605460eae0820ca8f46d7d5451bbf77
Anzhelika <darkl11n3ss@gmail.com>
2025-06-27 00:38:56 +0300

    Fix: install gunicorn globally for Render

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+
+export PATH="$HOME/.local/bin:$PATH"
+source "$HOME/.local/bin/env"
 
 uv venv .venv
 source .venv/bin/activate
 
 uv pip install .
+
 python manage.py collectstatic --noinput
 python manage.py migrate
+
