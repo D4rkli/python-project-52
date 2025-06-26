87aad6a5ad57fd1df513fc0b694787436d216f9b
Anzhelika <darkl11n3ss@gmail.com>
2025-06-26 23:11:44 +0300

    Fix uv install path

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
-
 source $HOME/.local/bin/env
 
-make install
-make collectstatic
-make migrate
+uv venv .venv
+source .venv/bin/activate
+
+pip install .
+
+python manage.py collectstatic --noinput
+python manage.py migrate
