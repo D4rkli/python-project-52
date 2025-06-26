032195bf0c9e43daf1b05f80a452b8cba97ef9be
Anzhelika <darkl11n3ss@gmail.com>
2025-06-26 23:50:53 +0300

    Fix uv install path

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -1,8 +1,11 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
 
 load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
@@ -11,8 +14,25 @@ ALLOWED_HOSTS = ['webserver']
 
 INSTALLED_APPS = [
     "django_bootstrap5",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+LANGUAGE_CODE = "en"
+USE_I18N = True
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
