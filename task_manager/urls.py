ab7674801ea2dda57b6617f73e944e809c422d6f
Anzhelika <darkl11n3ss@gmail.com>
2025-06-27 02:08:58 +0300

    Fix uv not found on Render

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import HomePageView
+from task_manager.views import HomePageView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,33 @@
-from django.views.generic.base import TemplateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .forms import UserForm
 
-class HomePageView(TemplateView):
-    template_name = "index.html"
+class UserListView(ListView):
+    model = User
+    template_name = 'users/user_list.html'
+    context_object_name = 'users'
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserForm
+    template_name = 'users/user_form.html'
+    success_url = reverse_lazy('login')
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'users/user_form.html'
+    success_url = reverse_lazy('users:list')
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = User
+    template_name = 'users/user_confirm_delete.html'
+    success_url = reverse_lazy('users:list')
+
+    def test_func(self):
+        return self.get_object() == self.request.user
