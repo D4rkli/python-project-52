bb9a14f1723ebbae727291d25ebb0d74d4072510
Anzhelika <darkl11n3ss@gmail.com>
2025-06-27 00:54:57 +0300

    Fix uv not found on Render

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ $HOME/.local/bin/uv venv .venv
 
 source .venv/bin/activate
 
-$HOME/.local/bin/uv pip install .
+$HOME/.local/bin/uv pip install --python .venv/bin/python .
 
 python manage.py collectstatic --noinput
 python manage.py migrate
