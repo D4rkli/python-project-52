d94b80e3022f50dda898aa10e79cacd456ece4cd
Anzhelika <darkl11n3ss@gmail.com>
2025-06-27 15:30:49 +0300

    Fix uv not found on Render

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -8,6 +8,16 @@ DATABASES = {
     "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'task_manager',
+]
+
 ALLOWED_HOSTS = ["webserver"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
