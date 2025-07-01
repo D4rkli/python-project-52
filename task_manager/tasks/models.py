a55ad82b377689bafb8920f3640ecfbcd0194cc1
Anzhelika <darkl11n3ss@gmail.com>
2025-07-02 02:46:53 +0300

    Fix uv not found on Render

---
+++ task_manager/tasks/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Label(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    labels = models.ManyToManyField(Label, blank=True, related_name='tasks')
+
+    def __str__(self):
+        return self.name
