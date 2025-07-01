c574de2986741206356b85ac332eb0124f49317e
Anzhelika <darkl11n3ss@gmail.com>
2025-07-02 00:22:45 +0300

    Fix uv not found on Render

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-def index(request):
-    return HttpResponse("Hello, it works!")
-
 urlpatterns = [
-    path('', index),
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
-
