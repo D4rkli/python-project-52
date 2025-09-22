d7b04d3236d353f61e42358709b3bcf31c27d08e
Anzhelika <darkl11n3ss@gmail.com>
2025-09-22 19:18:22 +0300

    Fix tests

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from django.contrib.auth.models import User
 
@@ -6,3 +6,17 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username')
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "placeholder": "Имя пользователя",
+            "id": "id_username"  # не обязательно, но удобно
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Пароль",
+            "id": "id_password"
+        })
+    )
