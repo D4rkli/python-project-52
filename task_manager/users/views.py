a86402fb6f77cfe45d0a2a829f31d6bdfcd7e5ce
Anzhelika <darkl11n3ss@gmail.com>
2025-09-24 03:39:31 +0300

    Fix tests

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,30 +1,34 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.db.models.deletion import ProtectedError
-from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
-from .forms import LoginForm
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-
 
 User = get_user_model()
 
+
 class AuthLoginView(LoginView):
     template_name = "users/login.html"
+
     def form_valid(self, form):
         messages.success(self.request, "Вы залогинены")
         return super().form_valid(form)
+
     def get_success_url(self):
         return reverse_lazy("home")
 
+
 class AuthLogoutView(LogoutView):
     next_page = reverse_lazy("home")
+
     def dispatch(self, request, *args, **kwargs):
         messages.success(request, "Вы разлогинены")
         return super().dispatch(request, *args, **kwargs)
+
 
 class UserListView(ListView):
     model = User
@@ -32,25 +36,28 @@ class UserListView(ListView):
     context_object_name = "users"
     ordering = ["id"]
 
+
 class UserCreateView(CreateView):
     form_class = UserCreationForm
     template_name = "users/create.html"
     success_url = reverse_lazy("login")
 
     def form_valid(self, form):
-        messages.success(self.request, "User was registered successfully")
+        messages.success(self.request, "Пользователь успешно зарегистрирован")
         return super().form_valid(form)
+
 
 class SelfOnlyMixin(UserPassesTestMixin):
     """Разрешаем update/delete только самому себе."""
+
     def test_func(self):
         obj = self.get_object()
         return self.request.user.is_authenticated and obj.pk == self.request.user.pk
 
     def handle_no_permission(self):
-        messages.error(self.request, "You have no rights to modify another user")
-        from django.shortcuts import redirect
+        messages.error(self.request, "У вас нет прав для изменения другого пользователя")
         return redirect("users_index")
+
 
 class UserUpdateView(LoginRequiredMixin, SelfOnlyMixin, UpdateView):
     model = User
@@ -59,8 +66,9 @@ class UserUpdateView(LoginRequiredMixin, SelfOnlyMixin, UpdateView):
     success_url = reverse_lazy("users_index")
 
     def form_valid(self, form):
-        messages.success(self.request, "User was updated successfully")
+        messages.success(self.request, "Пользователь успешно изменён")
         return super().form_valid(form)
+
 
 class UserDeleteView(LoginRequiredMixin, SelfOnlyMixin, DeleteView):
     model = User
@@ -70,8 +78,8 @@ class UserDeleteView(LoginRequiredMixin, SelfOnlyMixin, DeleteView):
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         try:
-            messages.success(self.request, "User was deleted successfully")
+            messages.success(self.request, "Пользователь успешно удалён")
             return super().post(request, *args, **kwargs)
         except ProtectedError:
-            messages.error(self.request, "Cannot delete user because it is in use")
+            messages.error(self.request, "Невозможно удалить пользователя, потому что он используется")
             return redirect("users_index")
