6ba4b79dad6c2f5543c8785de7171ea883f29928
Anzhelika <darkl11n3ss@gmail.com>
2025-09-28 22:29:16 +0300

    Fix tests

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,15 +1,25 @@
 from django import forms
 from .models import Task
-from task_manager.labels.models import Label
 
 class TaskForm(forms.ModelForm):
-    labels = forms.ModelMultipleChoiceField(
-        queryset=Label.objects.all(),
-        required=False,
-        widget=forms.SelectMultiple,
-    )
-
     class Meta:
         model = Task
-        fields = ["name", "description", "status", "executor", "labels"]
+        fields = ("name", "description", "status", "executor", "labels")
+        labels = {
+            "name": "Имя",
+            "description": "Описание",
+            "status": "Статус",
+            "executor": "Исполнитель",
+            "labels": "Метки",
+        }
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Имя"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "rows": 3, "placeholder": "Описание"}),
+            "status": forms.Select(attrs={"class": "form-select"}),
+            "executor": forms.Select(attrs={"class": "form-select"}),
+            "labels": forms.SelectMultiple(attrs={"class": "form-select"}),
+        }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""
