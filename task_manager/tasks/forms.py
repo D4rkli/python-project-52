ac0e97d3040e8013a742d387629f461a2062d686
Anzhelika <darkl11n3ss@gmail.com>
2025-09-28 22:43:46 +0300

    Fix tests

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -5,13 +5,6 @@ from .models import Task
 User = get_user_model()
 
 class TaskForm(forms.ModelForm):
-    executor = forms.ModelChoiceField(
-        queryset=User.objects.order_by("id"),
-        required=False,
-        label="Исполнитель",
-        widget=forms.Select(attrs={"class": "form-select"}),
-    )
-
     class Meta:
         model = Task
         fields = ("name", "description", "status", "executor", "labels")
@@ -19,16 +12,20 @@ class TaskForm(forms.ModelForm):
             "name": "Имя",
             "description": "Описание",
             "status": "Статус",
+            "executor": "Исполнитель",
             "labels": "Метки",
         }
         widgets = {
-            "name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Имя"}),
-            "description": forms.Textarea(attrs={"class": "form-control", "rows": 3, "placeholder": "Описание"}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control"}),
             "status": forms.Select(attrs={"class": "form-select"}),
+            "executor": forms.Select(attrs={"class": "form-select", "id": "id_executor"}),
             "labels": forms.SelectMultiple(attrs={"class": "form-select"}),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.label_suffix = ""
-        self.fields["executor"].queryset = User.objects.order_by("id")
+        self.fields["executor"].queryset = User.objects.all().order_by("id")
+        self.fields["executor"].required = False
+        self.fields["executor"].empty_label = "---------"
+        self.fields["executor"].label_from_instance = lambda u: u.username
