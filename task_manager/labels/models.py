148a56f054ff4aa42c0bd2015d960489361c9d58
Anzhelika <darkl11n3ss@gmail.com>
2025-09-29 01:15:24 +0300

    Fix tests

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 
 class Label(models.Model):
-    name = models.CharField("Имя", max_length=100, unique=True)
-    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+    name = models.CharField(max_length=255, unique=True)
 
-    class Meta:
-        ordering = ("id",)
-        verbose_name = "Метка"
-        verbose_name_plural = "Метки"
-
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
+
