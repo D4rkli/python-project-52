c3ce86d4eda70883d3c26d0375c8e10297023a70
Anzhelika <darkl11n3ss@gmail.com>
2025-09-28 19:49:35 +0300

    Fix tests

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,8 +1,13 @@
-from django import forms
 from .models import Status
+from django import forms
 
 class StatusForm(forms.ModelForm):
     class Meta:
         model = Status
-        fields = ["name"]
+        fields = ('name',)
+        labels = {'name': 'Имя'}
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Status(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField('Имя', max_length=255, unique=True)
 
     def __str__(self):
         return self.name
