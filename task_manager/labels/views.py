954287a3cae1f893d5fc2e632497689b01ddd079
Anzhelika <darkl11n3ss@gmail.com>
2025-09-28 22:08:49 +0300

    Fix tests

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,11 +1,14 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.db.models.deletion import ProtectedError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
 from .models import Label
 from .forms import LabelForm
+
 
 class LabelListView(LoginRequiredMixin, ListView):
     model = Label
@@ -13,37 +16,36 @@ class LabelListView(LoginRequiredMixin, ListView):
     context_object_name = "labels"
     ordering = ["id"]
 
-class LabelCreateView(LoginRequiredMixin, CreateView):
+
+class LabelCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Label
     form_class = LabelForm
     template_name = "labels/create.html"
     success_url = reverse_lazy("labels_index")
+    success_message = "Метка успешно создана"
 
-    def form_valid(self, form):
-        messages.success(self.request, "Метка успешно создана")
-        return super().form_valid(form)
 
-class LabelUpdateView(LoginRequiredMixin, UpdateView):
+class LabelUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Label
     form_class = LabelForm
     template_name = "labels/update.html"
     success_url = reverse_lazy("labels_index")
+    success_message = "Метка успешно изменена"
 
-    def form_valid(self, form):
-        messages.success(self.request, "Метка успешно изменена")
-        return super().form_valid(form)
 
 class LabelDeleteView(LoginRequiredMixin, DeleteView):
     model = Label
     template_name = "labels/delete.html"
     success_url = reverse_lazy("labels_index")
 
-    def delete(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         try:
-            response = super().delete(request, *args, **kwargs)
+            self.object.delete()
             messages.success(request, "Метка успешно удалена")
-            return response
         except ProtectedError:
-            messages.error(request, "Невозможно удалить метку, потому что она используется")
-            return redirect("labels_index")
+            messages.error(
+                request,
+                "Невозможно удалить метку, потому что она используется",
+            )
+        return redirect(self.success_url)
