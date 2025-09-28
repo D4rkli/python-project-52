83722dc6f3095d3298a08b6002200da450004cbe
Anzhelika <darkl11n3ss@gmail.com>
2025-09-29 01:56:42 +0300

    Fix tests

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,40 +1,36 @@
 import django_filters
-from django.contrib.auth import get_user_model
-from task_manager.statuses.models import Status
-from task_manager.labels.models import Label
+from django import forms
 from .models import Task
-
-User = get_user_model()
 
 class TaskFilter(django_filters.FilterSet):
     status = django_filters.ModelChoiceFilter(
-        field_name="status",
-        queryset=Status.objects.all(),
-        label="Status",
+        queryset=Task._meta.get_field("status").remote_field.model.objects.all().order_by("id"),
+        label="Статус",
+        widget=forms.Select(attrs={"class": "form-select"})
     )
     executor = django_filters.ModelChoiceFilter(
-        field_name="executor",
-        queryset=User.objects.all(),
-        label="Executor",
+        queryset=Task._meta.get_field("executor").remote_field.model.objects.all().order_by("id"),
+        label="Исполнитель",
+        widget=forms.Select(attrs={"class": "form-select"})
     )
-    label = django_filters.ModelChoiceFilter(
-        queryset=Label.objects.all(),
-        method="filter_by_label",
-        label="Label",
+    labels = django_filters.ModelChoiceFilter(
+        field_name="labels",
+        queryset=Task._meta.get_field("labels").remote_field.model.objects.all().order_by("id"),
+        label="Метка",
+        widget=forms.Select(attrs={"class": "form-select"})
     )
     self_tasks = django_filters.BooleanFilter(
-        method="filter_self_tasks",
-        label="Only my tasks",
+        method="filter_self",
+        label="Только свои задачи",
+        widget=forms.CheckboxInput(attrs={"class": "form-check-input", "id": "id_self_tasks"})
     )
 
     class Meta:
         model = Task
-        fields = ["status", "executor", "label", "self_tasks"]
+        fields = ["status", "executor", "labels", "self_tasks"]
 
-    def filter_by_label(self, qs, name, value):
-        return qs.filter(labels=value) if value else qs
-
-    def filter_self_tasks(self, qs, name, value):
-        if value and self.request.user.is_authenticated:
-            return qs.filter(author=self.request.user)
-        return qs
+    # показ только своих задач
+    def filter_self(self, queryset, name, value):
+        if value:
+            return queryset.filter(author=self.request.user)
+        return queryset
