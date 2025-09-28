eb02d39996be7e09266c0f26d257348577e05103
Anzhelika <darkl11n3ss@gmail.com>
2025-09-28 21:38:29 +0300

    Fix tests

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,21 +1,20 @@
 from django.contrib import messages
-from django.db.models.deletion import ProtectedError
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-
 from .models import Label
+from .forms import LabelForm
 
-
-class LabelListView(ListView):
+class LabelListView(LoginRequiredMixin, ListView):
     model = Label
     template_name = "labels/index.html"
     context_object_name = "labels"
+    ordering = ["id"]
 
-
-class LabelCreateView(CreateView):
+class LabelCreateView(LoginRequiredMixin, CreateView):
     model = Label
-    fields = ["name"]
+    form_class = LabelForm
     template_name = "labels/create.html"
     success_url = reverse_lazy("labels_index")
 
@@ -23,29 +22,25 @@ class LabelCreateView(CreateView):
         messages.success(self.request, "Метка успешно создана")
         return super().form_valid(form)
 
-
-class LabelUpdateView(UpdateView):
+class LabelUpdateView(LoginRequiredMixin, UpdateView):
     model = Label
-    fields = ["name"]
+    form_class = LabelForm
     template_name = "labels/update.html"
     success_url = reverse_lazy("labels_index")
 
     def form_valid(self, form):
-        messages.success(self.request, "Метка успешно изменена")
+        messages.success(self.request, "Метка успешно обновлена")
         return super().form_valid(form)
 
-
-class LabelDeleteView(DeleteView):
+class LabelDeleteView(LoginRequiredMixin, DeleteView):
     model = Label
     template_name = "labels/delete.html"
     success_url = reverse_lazy("labels_index")
 
-    def delete(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         self.object = self.get_object()
-        try:
-            response = super().delete(request, *args, **kwargs)
-            messages.success(request, "Метка успешно удалена")
-            return response
-        except ProtectedError:
-            messages.error(request, "Невозможно удалить метку, потому что она используется")
+        if self.object.tasks.exists():
+            messages.error(self.request, "Невозможно удалить метку, потому что она используется")
             return redirect("labels_index")
+        messages.success(self.request, "Label deleted successfully")
+        return super().post(request, *args, **kwargs)
