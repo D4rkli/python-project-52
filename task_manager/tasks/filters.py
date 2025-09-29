eaedfdd773b2c7c601ae40e0a32a920e44a7fdc5
Anzhelika <darkl11n3ss@gmail.com>
2025-09-29 12:01:12 +0300

    Fix tests

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,36 +1,50 @@
 import django_filters
-from django import forms
+from django.contrib.auth import get_user_model
+
 from .models import Task
+from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
+
+User = get_user_model()
+
 
 class TaskFilter(django_filters.FilterSet):
-    status = django_filters.ModelChoiceFilter(
-        queryset=Task._meta.get_field("status").remote_field.model.objects.all().order_by("id"),
-        label="Статус",
-        widget=forms.Select(attrs={"class": "form-select"})
-    )
-    executor = django_filters.ModelChoiceFilter(
-        queryset=Task._meta.get_field("executor").remote_field.model.objects.all().order_by("id"),
-        label="Исполнитель",
-        widget=forms.Select(attrs={"class": "form-select"})
-    )
-    labels = django_filters.ModelChoiceFilter(
-        field_name="labels",
-        queryset=Task._meta.get_field("labels").remote_field.model.objects.all().order_by("id"),
-        label="Метка",
-        widget=forms.Select(attrs={"class": "form-select"})
-    )
+    """Фильтр для списка задач."""
     self_tasks = django_filters.BooleanFilter(
-        method="filter_self",
+        method="filter_self_tasks",
         label="Только свои задачи",
-        widget=forms.CheckboxInput(attrs={"class": "form-check-input", "id": "id_self_tasks"})
+    )
+
+    status = django_filters.ModelChoiceFilter(
+        queryset=Status.objects.all().order_by("id"),
+        label="Статус",
+    )
+
+    executor = django_filters.ModelChoiceFilter(
+        queryset=User.objects.all().order_by("id"),
+        label="Исполнитель",
+    )
+
+    labels = django_filters.ModelMultipleChoiceFilter(
+        field_name="labels",
+        queryset=Label.objects.all().order_by("id"),
+        label="Метка",
     )
 
     class Meta:
         model = Task
-        fields = ["status", "executor", "labels", "self_tasks"]
+        fields = ("status", "executor", "labels", "self_tasks")
 
-    # показ только своих задач
-    def filter_self(self, queryset, name, value):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        def user_label(u: User) -> str:
+            full = (u.get_full_name() or "").strip()
+            return full or u.username
+
+        self.form.fields["executor"].label_from_instance = user_label
+
+    def filter_self_tasks(self, queryset, name, value):
         if value:
             return queryset.filter(author=self.request.user)
         return queryset
