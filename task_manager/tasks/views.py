d6b889c51666327052875241448ac9732bc40443
Anzhelika <darkl11n3ss@gmail.com>
2025-09-29 13:45:25 +0300

    Fix tests

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -68,13 +68,9 @@ class TaskUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class AuthorOnlyDeleteMixin(UserPassesTestMixin):
-
     def test_func(self):
         obj = self.get_object()
-        return (
-            self.request.user.is_authenticated
-            and obj.author_id == self.request.user.id
-        )
+        return self.request.user.is_authenticated and obj.author_id == self.request.user.id
 
     def handle_no_permission(self):
         messages.error(self.request, "У вас нет прав для удаления этой задачи")
@@ -86,7 +82,6 @@ class TaskDeleteView(LoginRequiredMixin, AuthorOnlyDeleteMixin, DeleteView):
     template_name = "tasks/delete.html"
     success_url = reverse_lazy("tasks_index")
 
-    def delete(self, request, *args, **kwargs):
-        response = super().delete(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
         messages.success(request, "Задача успешно удалена")
-        return response
+        return super().post(request, *args, **kwargs)
