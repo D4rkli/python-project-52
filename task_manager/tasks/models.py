c968d7b48624cb97a31ad623d0da882882e68c69
Anzhelika <darkl11n3ss@gmail.com>
2025-09-30 04:15:27 +0300

    Fix tests

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,27 +1,17 @@
-from django.conf import settings
 from django.db import models
+from django.conf import settings
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    status = models.ForeignKey(
-        'statuses.Status',
-        on_delete=models.PROTECT,          # защита от удаления
-        related_name='tasks',
-        verbose_name='Статус',
-    )
+    status = models.ForeignKey('statuses.Status', on_delete=models.PROTECT, related_name="tasks")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="authored_tasks")
+    labels = models.ManyToManyField('labels.Label', blank=True, related_name="tasks")
     executor = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        null=True, blank=True,
-        on_delete=models.SET_NULL,
-        related_name='executed_tasks',
-        verbose_name='Исполнитель',
-    )
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
-        related_name='authored_tasks',
-        verbose_name='Автор',
+        related_name="executed_tasks",
+        null=True, blank=True,
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
