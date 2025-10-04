f291d3452208a2e23fef735fa0ec7dc3e6a53a9d
Anzhelika <darkl11n3ss@gmail.com>
2025-10-05 02:41:20 +0300

    Fix tests

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.deletion import ProtectedError
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
@@ -7,39 +8,43 @@ from .models import Status
 from .forms import StatusForm
 
 
-class StatusListView(ListView):
+class StatusListView(LoginRequiredMixin, ListView):
     model = Status
     template_name = "statuses/index.html"
     context_object_name = "statuses"
     ordering = ["id"]
+    login_url = reverse_lazy("login")
 
 
-class StatusCreateView(CreateView):
+class StatusCreateView(LoginRequiredMixin, CreateView):
     model = Status
     form_class = StatusForm
     template_name = "statuses/create.html"
     success_url = reverse_lazy("statuses_index")
+    login_url = reverse_lazy("login")
 
     def form_valid(self, form):
         messages.success(self.request, "Статус успешно создан")
         return super().form_valid(form)
 
 
-class StatusUpdateView(UpdateView):
+class StatusUpdateView(LoginRequiredMixin, UpdateView):
     model = Status
     form_class = StatusForm
     template_name = "statuses/update.html"
     success_url = reverse_lazy("statuses_index")
+    login_url = reverse_lazy("login")
 
     def form_valid(self, form):
         messages.success(self.request, "Статус успешно изменен")
         return super().form_valid(form)
 
 
-class StatusDeleteView(DeleteView):
+class StatusDeleteView(LoginRequiredMixin, DeleteView):
     model = Status
     template_name = "statuses/delete.html"
     success_url = reverse_lazy("statuses_index")
+    login_url = reverse_lazy("login")
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
