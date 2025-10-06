6afebb76768417bcc847bf7af08eaab78de16d2c
Anzhelika <darkl11n3ss@gmail.com>
2025-10-06 14:23:42 +0300

    Fix tests

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.db.models.deletion import ProtectedError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
